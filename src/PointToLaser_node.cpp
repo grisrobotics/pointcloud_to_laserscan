6033c04505c8f03a695c719afc3bfe54d2523e3c
Minhyuk Kim <jeremy3771@naver.com>
2024-02-19 15:16:46 +0900

    Delete src directory

--- src/PointToLaser_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "pointcloudToLaserscan/PointToLaser.hpp"
-
-int main(int argc, char* argv[]) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PointToLaser>());
-    rclcpp::shutdown();
-    return 0;
-}
